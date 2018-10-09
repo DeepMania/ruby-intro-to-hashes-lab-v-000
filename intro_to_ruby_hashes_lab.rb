9e806b2f6f4ff8040a3bd586dc782e37e3031145
Yana Ustsinovich <yustinovich@icloud.com>
2018-10-09 01:51:09 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,14 @@ def monopoly
 		:railroads => {
 			pieces: 4,
 			:names => {},
-			:rent_in_dollars => {}
+			:rent_in_dollars => {
+				one_piece_owned: 25,
+				two_pieces_owned: 50,
+				three_pieces_owned: 100,
+				four_pieces_owned: 200,
+				:reading_railroad => {},
+				
+			}
 		}
 	}
 
